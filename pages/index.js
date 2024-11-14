c6a0e83e7b430059714e1c06f666222256777f94
vinicius <freitas.viniciuspk@outlook.com.br>
2024-11-14 03:50:28 +0000

    add: github and linkedin url's

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ const Home = () => {
   return (
     <>
       <h1>Seja muito bem-vindo(a)</h1>
+      <p>
+        Nossa missão é fazer um serviço de cardápio online que seja bom, barato
+        e que lhe atenda
+      </p>
+
+      <p>No mais, você pode me acompanhar em um dos seguintes links abaixo</p>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <a href="https://github.com/vinigofr">GitHub</a>
+        <a href="https://linkedin.com/in/vinigofr">LinkedIn</a>
+      </div>
     </>
   );
 };
