10f5e6d79e0cc9ab950687fdd7f4a123bf0ac5aa
vinicius <freitas.viniciuspk@outlook.com.br>
2024-12-08 17:49:02 +0000

    finish /api/v1/status and it's tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,32 @@
 import database from "infra/database";
 
 const status = async (req, res) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  res.status(200).json({ message: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionQuery = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionQuery.rows[0].server_version;
+
+  const dbMaxConnectionsQuery = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = parseInt(dbMaxConnectionsQuery.rows[0].max_connections);
+
+  const databaseName = req.query.databaseName;
+  const currentConnectionsQuery = await database.query({
+    text: "SELECT count(*)::int as openned_connections FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const currentConnectionsValue = currentConnectionsQuery.rows.length;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConnectionsValue,
+        openned_connections: currentConnectionsValue,
+      },
+    },
+  });
 };
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,26 @@ const PREFIX = "http://localhost:3000";
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch(`${PREFIX}/api/v1/status`);
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const responseDatabase = responseBody.dependencies.database;
+  expect(responseDatabase).toBeDefined();
+
+  expect(responseDatabase.version).toBeDefined();
+  expect(typeof responseDatabase.version).toBe("string");
+  expect(responseDatabase.version).toBe("16.0");
+
+  expect(responseDatabase.max_connections).toBeDefined();
+  expect(typeof responseDatabase.max_connections).toBe("number");
+
+  expect(responseDatabase.openned_connections).toBeDefined();
+  expect(typeof responseDatabase.openned_connections).toBe("number");
+  expect(responseDatabase.openned_connections).toBe(1);
+
+  console.log({ responseDatabase });
 });
