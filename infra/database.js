427bb8304f56460071e191c8adc41c3a4bf56a83
vinicius <freitas.viniciuspk@outlook.com.br>
2024-12-14 03:44:04 +0000

    adjust Postgres ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ const query = async (queryObject) => {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   try {
